a77d489d5b5e2dbb1b1ec66808d9eeedf70fb824
X659732 <111527272+X659732@users.noreply.github.com>
2022-09-09 22:19:07 -0400

    Update chapter1_10.js

--- Web115/styles/chapter1_10.js
+++ Web115/styles/chapter1_10.js
@@ -8,23 +8,24 @@ alert(company + greeting);
 
 var plants = 2;
 
-alert("We offer " + plants + "types of plants!");
+alert("We offer " + plants + " types of plants!");
 
 var choice1 = prompt("Would you like to hear about our plants? Enter y or n");
 
 if (choice1 === "y") {
 	var ornamental = 5.99;
-	var edible = 4.50;
-	alert("We offer a variety of edible plants for " + "$" + edible + " and ornamental plants for " + "$" + ornamental + ".");
-	var pay1 = parseFloat(prompt("How many edible plants would you be interested in?"));
+	var edible = 4.75;
+	alert("We offer a variety of fruit bearing plants for " + "$" + edible + " and herb plants for " + "$" + ornamental + ".");
+	var pay1 = parseFloat(prompt("How many fruit bearing plants would you be interested in?"));
 	var subTotal1 =  edible * pay1;
-	var pay2 = parseFloat(prompt("How many ornamental plants would you be interested in?"));
+	var pay2 = parseFloat(prompt("How many herb plants would you be interested in?"));
 	var subTotal2 =  edible * pay2;
 	var tax = (subTotal1 + subTotal2) * .06;
 	
 	var total = subTotal1 + subTotal2 + tax;
 	
 	alert("The total with tax included is " + total);
+	alert("Thank you for your patronage.");
 	
 }
 
