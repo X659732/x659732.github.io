15182e8dbee898ee0a618f70836f4fd36c9e0b6d
X659732 <111527272+X659732@users.noreply.github.com>
2022-09-26 09:53:10 -0400

    Update info2.js

--- Web115/styles/info2.js
+++ Web115/styles/info2.js
@@ -4,6 +4,7 @@
 window.onload = function() {
     
    document.forms[0].onsubmit = function() {
+	  document.getElementById("greeting").innerHTML= "Welcome to Tainted Toe Industries";
 	  var first_name=document.getElementById("fName").value;
 	  var middle_name=document.getElementById("mName").value;
 	  var last_name=document.getElementById("lName").value;
